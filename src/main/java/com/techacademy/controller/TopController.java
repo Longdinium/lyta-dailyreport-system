fc89fe0b2daa4f350616402245aa26f59659b1b9
Longdinium <rondobell0303@gmail.com>
2024-03-09 19:07:05 +0900

    ログイン後のリダイレクト先を変更

--- src/main/java/com/techacademy/controller/TopController.java
+++ src/main/java/com/techacademy/controller/TopController.java
@@ -2,6 +2,7 @@
 package com.techacademy.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -15,8 +16,11 @@ public class TopController {
 
     // ログイン後のトップページ表示
     @GetMapping(value = "/")
-    public String top() {
-        return "redirect:/employees";
+    public String top(Model model) {
+        // return "redirect:/employees";
+            // 修正前は従業員一覧画面にリダイレクト
+        return "redirect:/reports";
+            // 修正後は日報一覧画面にリダイレクト
     }
 
 }
