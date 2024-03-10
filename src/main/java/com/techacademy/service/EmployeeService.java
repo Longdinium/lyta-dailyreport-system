f2fa2ae56706596692e5c409391e279ab9690c51
Longdinium <rondobell0303@gmail.com>
2024-03-10 22:05:45 +0900

    従業員削除を修正

--- src/main/java/com/techacademy/service/EmployeeService.java
+++ src/main/java/com/techacademy/service/EmployeeService.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Service;
 
 import com.techacademy.constants.ErrorKinds;
 import com.techacademy.entity.Employee;
+import com.techacademy.entity.Report;
 import com.techacademy.repository.EmployeeRepository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,11 +22,13 @@ public class EmployeeService {
 
     private final EmployeeRepository employeeRepository;
     private final PasswordEncoder passwordEncoder;
+    private final ReportService reportService;
 
     @Autowired
-    public EmployeeService(EmployeeRepository employeeRepository, PasswordEncoder passwordEncoder) {
+    public EmployeeService(EmployeeRepository employeeRepository, PasswordEncoder passwordEncoder, ReportService reportService) {
         this.employeeRepository = employeeRepository;
         this.passwordEncoder = passwordEncoder;
+        this.reportService = reportService;
     }
 
     // 従業員保存
@@ -95,17 +98,17 @@ public class EmployeeService {
         employee.setUpdatedAt(now);
         employee.setDeleteFlg(true);
         
-        /* 削除対象の従業員に紐づいている日報情報の削除：ここから 
+        /* 削除対象の従業員に紐づいている日報情報の削除：ここから */
 
         // 削除対象の従業員（employee）に紐づいている、日報のリスト（reportList）を取得
-        List<Report> reportList = reportService.findByEmployee(employee);
+        List<Report> reportList = this.reportService.findByEmployee(employee);
 
         // 日報のリスト（reportList）を拡張for文を使って繰り返し
         for (Report report : reportList) {
             // 日報（report）のIDを指定して、日報情報を削除
-            reportService.delete(report.getId());
+            this.reportService.delete(String.valueOf( report.getId() ) );
         }
-        */
+        
 
         /* 削除対象の従業員に紐づいている日報情報の削除：ここまで */
 
